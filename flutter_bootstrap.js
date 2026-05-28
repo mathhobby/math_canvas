5336e11085cd58356d5d1077b9d2e6e8b29d5a3d
mathhobby <rkdanrjs21@gmail.com>
2026-05-28 19:33:58 +0900

    Fix: Perfectly resolve recursive pi/e rendering and clear boundary cursor overlay bug

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "12943653"
+    serviceWorkerVersion: "1266258421"
   }
 });
