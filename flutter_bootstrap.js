fde927749a714cce80e51be6a98581c894461693
mathhobby <rkdanrjs21@gmail.com>
2026-05-28 16:09:52 +0900

    Fix: Perfectly resolve LaTeX root tail layout overflow inside 380px sidebar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1113680539"
+    serviceWorkerVersion: "221600447"
   }
 });
